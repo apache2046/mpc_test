8e7abcce3abc6f49b855565103e41c3e356b3079
apache2046 <apache2046@gmail.com>
2022-11-23 22:00:41 +0800

    1

--- unicycle/acados/main.py
+++ unicycle/acados/main.py
@@ -3,18 +3,20 @@ from robot_model import export_robot_model
 import numpy as np
 import scipy.linalg
 from utils import plot_robot
+import sys
+import pathlib
 
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from common import pydraw
 
 X0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0])  # Intitalize the states [x,y,v,th,th_d]
 XN = np.array([3.0, 2.0, 0.0, 0.0, 0.0])
 
-N_horizon = 200  # Define the number of discretization steps
-T_horizon = 2.0  # Define the prediction horizon
-F_max = 10 * 0.2 # Define the max force allowed
-F_min = -10
-T_max = 1.0 * 1
-T_min = -1.0
-
+N_horizon = 400  # Define the number of discretization steps
+T_horizon = 4.0  # Define the prediction horizon
+F_max = 10  # Define the max force allowed
+T_max = 1.0 * 4
+Theta_max = 1.5
 
 
 def create_ocp_solver_description() -> AcadosOcp:
@@ -33,7 +35,7 @@ def create_ocp_solver_description() -> AcadosOcp:
     # set cost
     Q_mat = 1 * np.diag([1e3, 1e3, 1e3, 1e3, 0.0])  # [x, y, x_d, th, th_d]
     Qe_mat = 200 * np.diag([1e3, 1e3, 1e3, 1e3, 0.0])
-    R_mat = 0.1 * np.diag([1e-1, 1e-2])
+    R_mat = np.diag([1e-1, 1e-2])
 
     ocp.cost.cost_type = "LINEAR_LS"
     ocp.cost.cost_type_e = "LINEAR_LS"
@@ -61,8 +63,8 @@ def create_ocp_solver_description() -> AcadosOcp:
     ocp.constraints.ubu = np.array([+F_max, +T_max])
     ocp.constraints.idxbu = np.array([0, 1])
 
-    ocp.constraints.lbx = np.array([-1.5])
-    ocp.constraints.ubx = np.array([1.5])
+    ocp.constraints.lbx = np.array([-Theta_max])
+    ocp.constraints.ubx = np.array([+Theta_max])
     ocp.constraints.idxbx = np.array([2])
 
     ocp.constraints.x0 = X0
@@ -96,7 +98,7 @@ def closed_loop_simulation():
     )
 
     # prepare simulation
-    Nsim = 300
+    Nsim = 400
     nx = ocp.model.x.size()[0]
     nu = ocp.model.u.size()[0]
 
@@ -167,105 +169,7 @@ def closed_loop_simulation():
     )
 
     #print(simX)
-    pydraw(simX, simU)
-     
-def pydraw(X, U):
-
-    import pyglet
-    from pyglet import shapes
-
-    win_width, win_height = 640, 480
-    window = pyglet.window.Window(win_width, win_height)
-    batch = pyglet.graphics.Batch()
-
-    car_image = pyglet.image.load('car.png')
-    car_image.anchor_x = 32
-    car_image.anchor_y = 32
-    car = pyglet.sprite.Sprite(car_image, x=50, y=50)
-    car.rotation = 90
-
-    bg_color = shapes.Rectangle(0, 0, 960, 540, color=(255, 255, 255), batch=batch)
-    bg_lines = []
-    for i in list(range(win_width//32)):
-        bg_lines.append(shapes.Line((i+1)*32, 0, (i+1)*32, win_height, width=1, color=(180, 180, 180), batch=batch))
-    for i in list(range(win_height//32)):
-        bg_lines.append(shapes.Line(0, (i+1)*32, win_width, (i+1)*32, width=1, color=(180, 180, 180), batch=batch))
-    
-
-    start_point = shapes.Circle(100 + X0[0] * 100, 100 + X0[1] * 100, 3, color=(200, 40, 40), batch=batch)
-    end_point   = shapes.Circle(100 + XN[0] * 100, 100 + XN[1] * 100, 3, color=(200, 40, 40), batch=batch)
-
-    labelX = pyglet.text.Label('EgoX:',
-                            font_name='Times New Roman',
-                            font_size=10,
-                            bold=True,
-                            color=(0, 0, 0, 255),
-                            x=300, y=100, batch=batch)
-    labelY = pyglet.text.Label('EgoY:',
-                            font_name='Times New Roman',
-                            font_size=10,
-                            bold=True,
-                            color=(0, 0, 0, 255),
-                            x=300, y=80, batch=batch)
-    labelTheta = pyglet.text.Label('Theta:',
-                            font_name='Times New Roman',
-                            font_size=10,
-                            bold=True,
-                            color=(0, 0, 0, 255),
-                            x=300, y=60, batch=batch)
-    labelF = pyglet.text.Label('F:',
-                            font_name='Times New Roman',
-                            font_size=10,
-                            bold=True,
-                            color=(0, 0, 0, 255),
-                            x=400, y=100, batch=batch)
-    labelT = pyglet.text.Label('T:',
-                            font_name='Times New Roman',
-                            font_size=10,
-                            bold=True,
-                            color=(0, 0, 0, 255),
-                            x=400, y=80, batch=batch)
-    idx = 0
-    frame = 0
-    traj = []
-    @window.event
-    def on_draw():
-        nonlocal idx
-        nonlocal frame
-        nonlocal traj
-        frame += 1
-        if frame % 4 == 0:
-            idx = (idx + 1) % len(U)
-            if idx == 0:
-                traj = []
-        window.clear()
-        labelX.text = f"EgoX:  {X[idx, 0]:.2f}"
-        labelY.text = f"EgoY:  {X[idx, 1]:.2f}"
-        labelTheta.text = f"Theta:  {X[idx, 3]:.2f}"
-        labelF.text = f"F:  {U[idx, 0]:.2f}"
-        labelT.text = f"T:  {U[idx, 1]:.2f}"
-        batch.draw()
-
-        if len(traj) > 1:
-            batch1 = pyglet.graphics.Batch()
-            tmp = []
-            for x,y in traj:
-                tmp.append(shapes.Circle(x, y, 1, color=(160, 40, 40), batch=batch1))
-            batch1.draw()
-                
-
-
-        car.x = 100 + X[idx ,0] * 100
-        car.y = 100 + X[idx ,1] * 100
-        car.rotation = 90 - X[idx ,3] * 180 / np.pi
-        car.draw()
-        if len(traj) == 0 or traj[-1] != (car.x, car.y):
-            traj.append((car.x, car.y))
-        
-
-
-
-    pyglet.app.run()
+    pydraw(simX, simU, X0, XN)
 
 if __name__ == "__main__":
     closed_loop_simulation()
